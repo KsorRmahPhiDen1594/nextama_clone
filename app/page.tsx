fd3743f523efa46cf84eb0d59b189e2805efbcaf
KsorRmahPhiDen1594 <phiden1594@gmail.com>
2025-06-08 16:25:27 +0700

    update search_header_footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,16 @@
-    import { Button } from '@/components/ui/button'
+import React from "react";
+// import { RouteHandler } from 'react-router';
 
-    <Button>Button</Button>
-    
+import { Button } from "@/components/ui/button";
+import Header from "@/components/shared/header";
+import Footer from "@/components/shared/footer";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Header></Header>
+      <Button>Click me</Button>
+      <Footer></Footer>
+    </main>
+  );
+}
---
+++ lib/data.ts
@@ -0,0 +1,34 @@
+const data = {
+    headerMenus: [
+        {
+            name: "Today's Deals",
+            href: '/search?tag=todays-deals',
+        },
+        {
+            name: "New Arrivals",
+            href: '/search?tag=new-arrivals',
+        },
+        {
+            name: "Featured Products",
+            href: '/search?tag=featured',
+        },
+        {
+            name: "Browsing History",
+            href: '/#browsing-hostory',
+        },
+        {
+            name: "Customer Service",
+            href: '/page/customer-service',
+        },
+        {
+            name: "About Us",
+            href: '/page/about-us',
+        },
+        {
+            name: "Help",
+            href: '/page/help',
+        },
+    ],
+}
+
+export default data;
